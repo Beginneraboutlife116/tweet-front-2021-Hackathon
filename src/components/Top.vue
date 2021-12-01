086bd38f9a38bc42a1ac5b84153e9cd7088b8c9b
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-12-02 01:02:32 +0800

    feat/create-Top.vue

---
+++ src/components/Top.vue
@@ -0,0 +1,206 @@
+<template>
+  <section class="top-container">
+    <main class="top">
+      <header class="top__header">
+        <p class="top__header-title">Popular</p>
+      </header>
+      <div class="top__user" v-for="top in tops" :key="top.id">
+        <router-link :to="{path: '/:userId/profile', params: {userId: top.id}}">
+          <div class="top__user-avatar">
+            <img :src="top.image" class="top__user-avatar--img">
+          </div>
+        </router-link>
+        <div class="top__user-info">
+          <div>
+            <!-- route還沒設定，所以會失敗 -->
+            <router-link :to="{path: '/:userId/profile', params: {userId: top.id}}">
+              <p class="top__user-info--name"> {{top.name}} </p>
+            </router-link>
+            <router-link :to="{path: '/:userId/profile', params: {userId: top.id}}">
+              <p class="top__user-info--account"> @{{top.account}} </p>
+            </router-link>
+          </div>
+          <button class="top__user-info--follow" :class="{active: top.isFollowed}" @click.stop.prevent="toggleFollow(top.id)"> {{top.isFollowed ? '正在跟隨' : '跟隨'}} </button>
+        </div>
+      </div>
+    </main>
+  </section>
+</template>
+
+<script>
+const dummyData = [
+  {
+    id: 1,
+    account: 'user1',
+    name: 'user1',
+    img: null,
+    isFollowed: true
+  },
+  {
+    id: 2,
+    account: 'user2',
+    name: 'user2',
+    img: null,
+    isFollowed: true
+  },
+  {
+    id: 3,
+    account: '',
+    name: 'user3',
+    img: null,
+    isFollowed: false
+  },
+  {
+    id: 4,
+    account: 'user4',
+    name: 'user4',
+    img: null,
+    isFollowed: false
+  },
+  {
+    id: 5,
+    account: '',
+    name: 'user5',
+    img: null,
+    isFollowed: false
+  },
+  {
+    id: 6,
+    account: 'user6',
+    name: 'user6',
+    img: null,
+    isFollowed: false
+  },
+  {
+    id: 7,
+    account: 'user7',
+    name: 'user7',
+    img: null,
+    isFollowed: false
+  },
+  {
+    id: 8,
+    account: 'user8',
+    name: 'user8',
+    img: null,
+    isFollowed: true
+  },
+  {
+    id: 9,
+    account: '',
+    name: 'user9',
+    img: null,
+    isFollowed: true
+  },
+  {
+    id: 10,
+    account: 'user10',
+    name: 'user10',
+    img: null,
+    isFollowed: true
+  }
+]
+
+export default {
+  name: 'Top',
+  data () {
+    return {
+      tops: []
+    }
+  },
+  methods: {
+    fetchTops () {
+      // TODO: 與後端串接tops的資料
+      this.tops = dummyData.map(data => {
+        return {
+          ...data,
+          account: data.account || '---',
+          image: data.image || 'https://via.placeholder.com/350x220/DFDFDF?text=No+Image'
+        }
+      })
+    },
+    toggleFollow (userId) {
+      // TODO: 將資料傳給後端
+      this.tops = this.tops.map(top => {
+        if (top.id === userId) {
+          return {
+            ...top,
+            isFollowed: !top.isFollowed
+          }
+        }
+        return {
+          ...top
+        }
+      })
+    }
+  },
+  created () {
+    this.fetchTops()
+  }
+}
+</script>
+
+<style lang="scss">
+.top-container {
+  grid-column: 2;
+  padding: 1.5rem 0 0 3rem;
+  border-left: 1px solid var(--border-color);
+}
+
+.top {
+  width: 75%;
+  border-radius: 1.4rem;
+  background-color: var(--fill-color);
+  box-shadow: 0 4px 4px 0 hsla(0, 0, 0, 25%);
+  &__header {
+    padding: 1rem 0 1rem 1.5rem;
+    &-title {
+      font-size: $font-lg;
+      font-weight: bold;
+    }
+  }
+  &__user {
+    display: flex;
+    height: 7rem;
+    padding: 1rem 1.5rem;
+    border-top: 1px solid var(--border-color);
+    &-avatar {
+      width: 5rem;
+      height: 5rem;
+      border-radius: 50%;
+      margin-right: 1rem;
+      &--img {
+        border-radius: 50%;
+        object-fit: cover;
+      }
+    }
+    &-info {
+      flex: 1 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      &--name {
+        color: var(--font-color);
+      }
+      &--name:hover {
+        color: var(--main-color);
+      }
+      &--name,
+      &--account {
+        font-size: $font-md;
+        line-height: $font-md;
+        font-weight: bold;
+      }
+      &--account {
+        color: var(--label-color);
+      }
+      &--account:hover {
+        color: var(--main-color);
+      }
+      &--follow {
+        @extend %buttonStyle;
+      }
+    }
+  }
+}
+</style>
