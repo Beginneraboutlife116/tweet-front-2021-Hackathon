13371ecb2ebd7b045ae0065eca959e33456fa0f3
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-12-02 01:35:30 +0800

    finish HTML structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import SignIn from './../views/SignIn'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'sign-in',
+    component: SignIn
+  },
   {
     path: '/tweets',
     name: 'tweets',
---
+++ src/views/SignIn.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="sign-container">
+    <div class="sign">
+      <header class="sign__header">
+        <div class="sign__header-logo"></div>
+        <p class="sign__header-title"><span>登入</span><span>Alphitter</span></p>
+      </header>
+      <label class="sign__form-row">
+        <p class="sign__form-title"></p>
+        <input type="email" class="sign__form-input">
+        <p class="sign__form-error">
+          <span class="error"></span>
+          <span class="limit"></span>
+        </p>
+      </label>
+      <label class="sign__form-row">
+        <p class="sign__form-title"></p>
+        <input type="password" class="sign__form-input">
+        <p class="sign__form-error">
+          <span class="error"></span>
+          <span class="limit"></span>
+        </p>
+      </label>
+      <button class="sign__submit" type="submit">登入</button>
+      <div class="sign__btns">
+        <router-link to="" class="sign__link"></router-link>
+        <router-link to="" class="sign__link"></router-link>
+      </div>
+    </div>
+  </div>
+</template>
