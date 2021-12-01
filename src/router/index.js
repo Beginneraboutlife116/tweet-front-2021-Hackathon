35287a49f9412f17746231425b36e9b0f0d6b021
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-12-01 21:25:01 +0800

    chore/set-layout

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,17 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/tweets',
+    name: 'tweets',
+    component: () => import('./../views/Main'),
+    children: [
+      {
+        path: '',
+        component: () => import('./../views/Tweets')
+      }
+    ]
+  }
 ]
 
 const router = new VueRouter({
