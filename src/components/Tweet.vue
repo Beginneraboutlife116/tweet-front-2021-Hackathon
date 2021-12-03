45a8a40e8f4669c4621991fe51fc7f3b4538b5d4
carolebot <carol.tang79@gmail.com>
2021-12-03 21:01:18 +0800

    feat/create-tweet layout and add like toggle

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,62 +1,84 @@
 <template>
-  <div class="tweet-container">
-    <div class="tweet">
-      <router-link :to="{ name:'profile'}" , params: { id: user.id }>
-        <div class="tweet__avatar">
-          <img class="tweet__avatar-img" src="" alt="">
-        </div>
-      </router-link>
-      <div class="tweet__info">
-        <div class="tweet__info-postBy">
-          <!-- 點擊貼文中使用者頭像/name/account時，能到profile頁 -->
-          <router-link :to="{ name:'profile'}" , params: { id: user.id }>
-            <span class="tweet__info-postBy--name"></span>
-            <span class="tweet__info-postBy--account"></span>
-          </router-link>
-          <!-- 點擊時間連到當則推文 -->
-          <router-link :to="{name:'tweet'}" , params: { id: tweet.id }>
-            <span class="tweet__info-postBy--timeStamp"></span>
-          </router-link>
-        </div>
-        <!-- 點擊推文內容到當則推文 -->
-        <router-link :to="{name:'tweet'}" , params: { id: tweet.id }>
-          <p class="tweet__info-desc"></p>
+  <div>
+    <div class="tweet-container">
+      <div class="tweet">
+        <!-- 點擊貼文中使用者頭像/name/account時，能到profile頁 -->
+        <router-link :to="`/home/${tweet.user.id}`" class="tweet__avatar">
+          <img
+            class="tweet__avatar--img"
+            :src="tweet.user.avatar || 'https://fakeimg.pl/300/'"
+            alt="avatar"
+            aria-label="avatar"
+          />
         </router-link>
-        <div class="tweet__info-count">
-          <router-link>
-          <span>
-            <svg
-              width="30"
-              height="30"
-              viewBox="0 0 30 30"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M17.5576 2.80254L12.3726 2.79004H12.3701C6.90262 2.79004 2.62012 7.07379 2.62012 12.5425C2.62012 17.665 6.60262 21.55 11.9514 21.755V26.54C11.9514 26.675 12.0064 26.8975 12.1014 27.0438C12.2789 27.325 12.5814 27.4775 12.8914 27.4775C13.0639 27.4775 13.2376 27.43 13.3939 27.33C13.7239 27.12 21.4851 22.155 23.5039 20.4475C25.8814 18.435 27.3039 15.485 27.3076 12.5575V12.5363C27.3001 7.07754 23.0201 2.80254 17.5576 2.80129V2.80254ZM22.2914 19.0175C20.8739 20.2175 16.2139 23.2738 13.8264 24.8213V20.8375C13.8264 20.32 13.4076 19.9 12.8889 19.9H12.3939C7.81887 19.9 4.49637 16.805 4.49637 12.5425C4.49637 8.12504 7.95637 4.66504 12.3714 4.66504L17.5551 4.67754H17.5576C21.9726 4.67754 25.4326 8.13504 25.4351 12.5475C25.4314 14.935 24.2576 17.3525 22.2926 19.0175H22.2914Z"
-                fill="#657786"
-              />
-            </svg>
-            <span class="tweet__info-count--reply"></span>
-          </span>
-          </router-link>
-          <span>
-            <router-link>
-            <svg
-              width="30"
-              height="30"
-              viewBox="0 0 30 30"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 27.0473H14.9825C11.7537 26.9873 2.4375 18.5698 2.4375 10.5973C2.4375 6.76728 5.59375 3.40479 9.19125 3.40479C12.0537 3.40479 13.9787 5.37979 14.9987 6.81729C16.0162 5.38229 17.9412 3.40479 20.805 3.40479C24.405 3.40479 27.56 6.76729 27.56 10.5985C27.56 18.5685 18.2425 26.986 15.0137 27.0448H15V27.0473ZM9.1925 5.28104C6.5925 5.28104 4.31375 7.76603 4.31375 10.5998C4.31375 17.7748 13.1062 25.0948 15.0012 25.1723C16.8987 25.0948 25.6887 17.776 25.6887 10.5998C25.6887 7.76603 23.41 5.28104 20.81 5.28104C17.65 5.28104 15.885 8.95104 15.87 8.98729C15.5825 9.68979 14.425 9.68979 14.1362 8.98729C14.1187 8.94979 12.355 5.28104 9.19375 5.28104H9.1925Z"
-                fill="#657786"
-              />
-            </svg>
-            <span class="tweet__info-count--like"></span>
+        <div class="tweet__info">
+          <div class="tweet__info-postBy">
+            <!-- 點擊貼文中使用者頭像/name/account時，能到profile頁 -->
+            <router-link :to="`/home/${tweet.user.id}`">
+              <span class="name">{{ tweet.user.name }}</span>
+              <span class="account">@{{ tweet.user.account }}・</span>
             </router-link>
-          </span>
+            <!-- 點擊時間連到當則推文 -->
+            <router-link :to="`/home/${tweet.id}`">
+              <span class="timeStamp">{{ tweet.created_at | fromNow }}</span>
+            </router-link>
+          </div>
+          <!-- 點擊推文內容到當則推文 -->
+          <router-link :to="`/home/${tweet.id}`">
+            <p class="tweet__info-description">{{ tweet.description }}</p>
+          </router-link>
+          <div class="tweet__info-count">
+            <!-- 點擊回覆打開回覆modal -->
+            <span @click.prevent.stop="toggleReplyModal">
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.77881 1.40127L6.18631 1.39502H6.18506C3.45131 1.39502 1.31006 3.53689 1.31006 6.27127C1.31006 8.83252 3.30131 10.775 5.97568 10.8775V13.27C5.97568 13.3375 6.00318 13.4488 6.05068 13.5219C6.13943 13.6625 6.29068 13.7388 6.44568 13.7388C6.53193 13.7388 6.61881 13.715 6.69693 13.665C6.86193 13.56 10.7426 11.0775 11.7519 10.2238C12.9407 9.21752 13.6519 7.74252 13.6538 6.27877V6.26814C13.6501 3.53877 11.5101 1.40127 8.77881 1.40064V1.40127ZM11.1457 9.50877C10.4369 10.1088 8.10693 11.6369 6.91318 12.4106V10.4188C6.91318 10.16 6.70381 9.95002 6.44443 9.95002H6.19693C3.90943 9.95002 2.24818 8.40252 2.24818 6.27127C2.24818 4.06252 3.97818 2.33252 6.18568 2.33252L8.77756 2.33877H8.77881C10.9863 2.33877 12.7163 4.06752 12.7176 6.27377C12.7157 7.46752 12.1288 8.67627 11.1463 9.50877H11.1457Z"
+                  fill="#657786"
+                />
+              </svg>
+              <span class="tweet__info-count--reply">{{
+                tweet.replyCounts
+              }}</span>
+            </span>
+            <!-- 點擊喜歡愛心亮起 -->
+            <span @click.prevent.stop="toggleLikeModal(tweet.id)">
+              <svg
+                v-show="!tweet.isLike"
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7.5 13.5239H7.49125C5.87687 13.4939 1.21875 9.28514 1.21875 5.29889C1.21875 3.38389 2.79687 1.70264 4.59562 1.70264C6.02687 1.70264 6.98937 2.69014 7.49937 3.40889C8.00812 2.69139 8.97062 1.70264 10.4025 1.70264C12.2025 1.70264 13.78 3.38389 13.78 5.29951C13.78 9.28451 9.12125 13.4933 7.50687 13.5226H7.5V13.5239ZM4.59625 2.64076C3.29625 2.64076 2.15687 3.88326 2.15687 5.30014C2.15687 8.88764 6.55312 12.5476 7.50062 12.5864C8.44937 12.5476 12.8444 8.88826 12.8444 5.30014C12.8444 3.88326 11.705 2.64076 10.405 2.64076C8.825 2.64076 7.9425 4.47576 7.935 4.49389C7.79125 4.84514 7.2125 4.84514 7.06812 4.49389C7.05937 4.47514 6.1775 2.64076 4.59687 2.64076H4.59625Z"
+                  fill="#657786"
+                />
+              </svg>
+              <svg
+                v-show="tweet.isLike"
+                width="22"
+                height="20"
+                viewBox="0 0 22 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11 19.6381H10.986C8.40295 19.5901 0.949951 12.8561 0.949951 6.47812C0.949951 3.41412 3.47495 0.724121 6.35295 0.724121C8.64295 0.724121 10.183 2.30412 10.999 3.45412C11.813 2.30612 13.353 0.724121 15.644 0.724121C18.524 0.724121 21.048 3.41412 21.048 6.47912C21.048 12.8551 13.594 19.5891 11.011 19.6361H11V19.6381Z"
+                  fill="#E0245E"
+                />
+              </svg>
+              <span class="tweet__info-count--like">{{
+                tweet.likeCounts
+              }}</span>
+            </span>
+          </div>
         </div>
       </div>
     </div>
@@ -64,11 +86,123 @@
 </template>
 
 <script>
+import { fromNowFilter } from './../mixins/helpers'
 export default {
+  mixins: [fromNowFilter],
   name: 'Tweet',
-  props: {}
+  props: {
+    initialTweet: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      tweet: {
+        id: -1,
+        description: '',
+        created_at: '2001-12-02T16:44:25.000Z',
+        user: {
+          id: -1,
+          account: 'NoAccount',
+          name: 'NoName',
+          avatar: ''
+        },
+        isLike: true,
+        likeCounts: 100,
+        replyCounts: 20
+      }
+    }
+  },
+  created () {
+    this.fetchTweet()
+  },
+  methods: {
+    toggleReplyModal () {
+      // 開啟modal
+    },
+    toggleLikeModal (tweetId) {
+      this.tweet = {
+        ...this.tweet,
+        isLike: !this.tweet.isLike
+      }
+      if (this.tweet.isLike === true) {
+        this.tweet.likeCounts++
+      } else {
+        this.tweet.likeCounts--
+      }
+    },
+    fetchTweet () {
+      this.tweet = {
+        ...this.initialTweet
+      }
+    }
+  },
+  watch: {
+    initialTweet (newValue) {
+      this.tweet = {
+        ...this.tweet,
+        ...newValue
+      }
+    }
+  }
 }
 </script>
 
-<style lang="sass">
+<style lang="scss">
+.tweet-container {
+  width: 60rem;
+}
+.tweet {
+  display: flex;
+  padding: 1rem 1.5rem;
+  border: 1px solid;
+  border-color: transparent var(--border-color) var(--border-color)
+    var(--border-color);
+  &__avatar {
+    margin-right: 1rem;
+    img {
+      max-width: 5rem;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+  }
+
+  &__info {
+    &-postBy {
+      font-size: $font-md;
+      line-height: $font-md;
+      .name {
+        margin-right: 0.5rem;
+        font-weight: bold;
+        color: var(--font-color);
+      }
+      .account,
+      .timeStamp {
+        color: var(--label-color);
+      }
+    }
+    &-description {
+      padding: 0.6rem 0 1rem 0;
+      color: var(--font-color);
+    }
+    &-count {
+      display: flex;
+      width: 13rem;
+      span {
+        display: flex;
+        align-items: center;
+        font-size: $font-xs;
+        justify-content: flex-start;
+        svg {
+          margin: 0 1rem 0 0;
+          height: 1.5rem;
+        }
+      }
+      .tweet__info-count--reply {
+        margin-right: 5rem;
+      }
+    }
+  }
+}
 </style>
--- src/views/Tweets.vue
+++ src/views/Tweets.vue
@@ -1,21 +1,102 @@
 <template>
   <div class="user-container">
     <header class="user-header">
-      <img src="" alt="">
+      <img src="" alt="" />
     </header>
-  <Tweet/>
+    <Tweet  v-for="tweet in tweets" :key="tweet.id" :initial-tweet="tweet" />
   </div>
 </template>
 
 <script>
 import Tweet from './../components/Tweet'
+
+const dummyData = {
+  tweets: [
+    {
+      id: 1,
+      description:
+        'amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sitLorem ipsum dolor sit amet',
+      created_at: '2021-12-02T16:44:25.000Z',
+      user: {
+        id: 2,
+        account: 'account',
+        name: 'userName',
+        avatar: null
+      },
+      isLike: true,
+      likeCounts: 100,
+      replyCounts: 20
+    },
+    {
+      id: 2,
+      description:
+        'amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit',
+      created_at: '2021-12-02T16:44:25.000Z',
+      user: {
+        id: 2,
+        account: 'account2',
+        name: 'userName2',
+        avatar: null
+      },
+      isLike: false,
+      likeCounts: 30,
+      replyCounts: 50
+    },
+    {
+      id: 3,
+      description:
+        'amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit',
+      created_at: '2021-12-02T16:44:25.000Z',
+      user: {
+        id: 3,
+        account: 'account3',
+        name: 'userName3',
+        avatar: null
+      },
+      isLike: false,
+      likeCounts: 2,
+      replyCounts: 5
+    },
+    {
+      id: 4,
+      description:
+        'amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit',
+      created_at: '2021-12-02T16:44:25.000Z',
+      user: {
+        id: 4,
+        account: '',
+        name: '',
+        avatar: null
+      },
+      isLike: false,
+      likeCounts: 4,
+      replyCounts: 5
+    }
+  ]
+}
 export default {
   components: {
     Tweet
+  },
+  data () {
+    return {
+      tweets: []
+    }
+  },
+  created () {
+    this.fetchTweets()
+  },
+  methods: {
+    fetchTweets () {
+      this.tweets = dummyData.tweets.map((data) => {
+        return {
+          ...data
+        }
+      })
+    }
   }
 }
 </script>
 
 <style lang="scss">
-
 </style>
