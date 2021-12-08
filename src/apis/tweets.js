6c752066c2f5dd6702b0ca1bff8d539ea9479e67
carolebot <carol.tang79@gmail.com>
2021-12-08 21:13:04 +0800

    fix/clean merge system words

--- src/apis/tweets.js
+++ src/apis/tweets.js
@@ -7,15 +7,13 @@ export default {
   postTweets ({ description }) {
     return apiHelper.post('/tweets/', { description })
   },
-<<<<<<< HEAD
   postLike (tweetId) {
     return apiHelper.post(`/tweets/${tweetId}/like`)
   },
   postUnlike (tweetId) {
     return apiHelper.post(`/tweets/${tweetId}/unlike`)
-=======
+  },
   getSingleTweet ({ tweetId }) {
     return apiHelper.get(`/tweets/${tweetId}`)
->>>>>>> main
   }
 }
