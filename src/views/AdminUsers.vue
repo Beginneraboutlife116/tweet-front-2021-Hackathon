bb89f2b1a25b2174c464f38b863a956f45a79734
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-12-09 12:56:39 +0800

    finish HTML structure

--- src/views/AdminUsers.vue
+++ src/views/AdminUsers.vue
@@ -1,3 +1,131 @@
 <template>
-  <h1>i'm adminusers</h1>
+  <section class="adminUsers">
+    <header class="adminUsers-header">使用者列表</header>
+    <main class="cards">
+      <div class="card">
+        <div class="card__cover">
+          <img src="" alt="" class="card__cover-img">
+        </div>
+        <div class="card__intro">
+          <div class="card__avatar">
+            <img src="" alt="" class="card__avatar-img">
+          </div>
+          <p class="card__name">test</p>
+          <p class="card__account">@test</p>
+          <p class="card__counts">
+            <span class="card__counts-tweets">
+              <!-- svg -->
+              <strong>123</strong>
+            </span>
+            <span class="card__counts-likes">
+              <!-- svg -->
+              <strong>321</strong>
+            </span>
+          </p>
+          <p class="card__follow">
+            <span class="card__follow-following">
+              <strong>A</strong> 個跟隨中
+            </span>
+            <span class="card__follow-follower">
+              <strong>B</strong> 位跟隨者
+            </span>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card__cover">
+          <img src="" alt="" class="card__cover-img">
+        </div>
+        <div class="card__intro">
+          <div class="card__avatar">
+            <img src="" alt="" class="card__avatar-img">
+          </div>
+          <p class="card__name">test</p>
+          <p class="card__account">@test</p>
+          <p class="card__counts">
+            <span class="card__counts-tweets">
+              <!-- svg -->
+              <strong>123</strong>
+            </span>
+            <span class="card__counts-likes">
+              <!-- svg -->
+              <strong>321</strong>
+            </span>
+          </p>
+          <p class="card__follow">
+            <span class="card__follow-following">
+              <strong>A</strong> 個跟隨中
+            </span>
+            <span class="card__follow-follower">
+              <strong>B</strong> 位跟隨者
+            </span>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card__cover">
+          <img src="" alt="" class="card__cover-img">
+        </div>
+        <div class="card__intro">
+          <div class="card__avatar">
+            <img src="" alt="" class="card__avatar-img">
+          </div>
+          <p class="card__name">test</p>
+          <p class="card__account">@test</p>
+          <p class="card__counts">
+            <span class="card__counts-tweets">
+              <!-- svg -->
+              <strong>123</strong>
+            </span>
+            <span class="card__counts-likes">
+              <!-- svg -->
+              <strong>321</strong>
+            </span>
+          </p>
+          <p class="card__follow">
+            <span class="card__follow-following">
+              <strong>A</strong> 個跟隨中
+            </span>
+            <span class="card__follow-follower">
+              <strong>B</strong> 位跟隨者
+            </span>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card__cover">
+          <img src="" alt="" class="card__cover-img">
+        </div>
+        <div class="card__intro">
+          <div class="card__avatar">
+            <img src="" alt="" class="card__avatar-img">
+          </div>
+          <p class="card__name">test</p>
+          <p class="card__account">@test</p>
+          <p class="card__counts">
+            <span class="card__counts-tweets">
+              <!-- svg -->
+              <strong>123</strong>
+            </span>
+            <span class="card__counts-likes">
+              <!-- svg -->
+              <strong>321</strong>
+            </span>
+          </p>
+          <p class="card__follow">
+            <span class="card__follow-following">
+              <strong>A</strong> 個跟隨中
+            </span>
+            <span class="card__follow-follower">
+              <strong>B</strong> 位跟隨者
+            </span>
+          </p>
+        </div>
+      </div>
+    </main>
+  </section>
 </template>
+
+<style lang="scss">
+
+</style>
