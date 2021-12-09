58879644990ff3bd4615d0d32d10a6bab3b8c749
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-12-09 22:51:43 +0800

    deploy success

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.Github.io/<REPO>
 # git push -f https://github.com/<USERNAME>/<REPO>.git main:gh-pages
-git push -f https://github.com/Beginneraboutlife116/tweet-front-2021.git main:gh-pages
+git push -f https://github.com/Beginneraboutlife116/tweet-front-2021.git master:gh-pages
 
 cd -
