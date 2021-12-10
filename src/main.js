9162ce69c81d331a04b04c8a2925d6e87706d2c8
carolebot <carol.tang79@gmail.com>
2021-12-10 17:35:35 +0800

    merge layout

--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
+
+// vue-socket.io範例
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://metinseylan.com:1992',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  },
+  options: { path: '//' }
+}))
 
 new Vue({
   router,
